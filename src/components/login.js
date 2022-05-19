64ba0767550ddcd70459a366c1b112d76a7360a2
SazzadulIslamCr <sazzadul@coderex.co>
2022-05-19 16:00:39 +0600

    loginPage modify

--- src/components/login.js
+++ src/components/login.js
@@ -1,41 +1,37 @@
 import React from 'react'
-import './login.module.css'
+import {Col, Container, Row, Form, Button} from 'react-bootstrap'
+import './login.css'
+import { BiUserCircle } from 'react-icons/bi';
+import uiImg from '../images/login_page.svg';
 
 function login() {
   return (
-    <div>
-        <div className="card o-hidden border-0 shadow-lg my-5">
-        <div className="card-body p-0">
-            {/* Nested Row within Card Body */}
-            <div className="row">
-            <div className="col-lg-6 d-none d-lg-block bg-login-image" />
-            <div className="col-lg-6">
-                <div className="p-5">
-                <div className="text-center">
-                    <h1 className="h4 text-gray-900 mb-4" style={{color: 'red'}}>CodeRex EMS</h1>
-                </div>
-                <form className="user">
-                    <div className="form-group" style={{margin: '2px'}}>
-                        <input type="email" className="form-control form-control-user" id="email" aria-describedby="emailHelp" placeholder="Enter Email Address..." />
-                    </div>
-                    <div className="form-group" style={{margin: '2px'}}>
-                        <input type="password" className="form-control form-control-user" id="exampleInputPassword" placeholder="Password" />
-                    </div>
-                    <div className="form-group">
-                    </div>
-                    <div className='col text-center'>
-                        <button className="btn btn-success btn-user btn-block">
-                            Sign In
-                        </button>
-                    </div>
-                </form>
-                </div>
-            </div>
-            </div>
-        </div>
-        </div>
+    <>
+      <Container className='m-5'>
+        <Row>
+            <Col lg={6} md={6} className='text-center mt-5 p-3'>
+                <BiUserCircle size='60px' className='loginIcon'/>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
 
-    </div>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="btn btn-primary w-100" type="submit">
+                        Sign In
+                    </Button>
+                    {/* forgot-password can be added later here */}
+                </Form>
+            </Col> 
+            <Col lg={6} md={6}>
+                <img className='svg' src={uiImg} alt=''/>
+            </Col>
+        </Row>
+      </Container>  
+
+    </>
   )
 }
 
