a9fba8422691c011ea0083d576003163de48ecac
SazzadulIslamCr <sazzadul@coderex.co>
2022-05-23 18:04:21 +0600

    some-item-inserted-dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,28 +1,110 @@
 import {useEffect,useState,Component,React} from 'react';
 import PieChart from '../components/admin/PieChart';
 import SidebarAdmin from '../components/admin/SidebarAdmin'
-// import {Card, Container, Col, Row, Form, Button} from 'react-bootstrap'
-// import { BiUserCircle } from 'react-icons/bi';
-// import uiImg from '../images/login_page.svg';
-// import TestData from '../data/test.json'
+import {Card, Container, Col, Row, Form, Button} from 'react-bootstrap'
+import { BiUserCircle } from 'react-icons/bi';
+import uiImg from '../images/login_page.svg';
+import TestData from '../data/test.json'
 
 const Dashboard = () => {
+  const[record,setRecord] = useState([])
+ 
+   const getData = () =>
+   {
+       fetch('https://jsonplaceholder.typicode.com/users')
+       .then(resposne=> resposne.json())
+       .then(res=>setRecord(res))
+   }
+ 
+   useEffect(() => {
+      getData();
+   },)
+
    return (
    <div style={{color: 'black'}} className="col main pt-5 mt-3">
        <p style={{color: 'black'}} className="lead d-none d-sm-block">Equipment Details and Requisitions</p>
 
-       <div className="alert alert-warning fade collapse" role="alert" id="myAlert">
-            <button type="button" className="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">×</span>
-                <span className="sr-only">Close</span>
-            </button>
-            <strong>Data and Records</strong> Learn more
+        <div className="row mb-3">
+            <div className="col-xl-3 col-sm-6 py-2">
+                <div className="card bg-success text-white h-40">
+                    <div className="card-body bg-success" style={{backgroundColor:"#57b960"}}>
+                        <div className="rotate">
+                            <i className="fa fa-user fa-4x"></i>
+                        </div>
+                        <h6 className="text-uppercase">Users</h6>
+                        <h1 className="display-4">134</h1>
+                    </div>
+                </div>
+            </div>
+            <div className="col-xl-3 col-sm-6 py-2">
+                <div className="card text-white bg-danger h-40">
+                    <div className="card-body bg-danger">
+                        <div className="rotate">
+                            <i className="fa fa-list fa-4x"></i>
+                        </div>
+                        <h6 className="text-uppercase">Posts</h6>
+                        <h1 className="display-4">87</h1>
+                    </div>
+                </div>
+            </div>
+            <div className="col-xl-3 col-sm-6 py-2">
+                <div className="card text-white bg-info h-40">
+                    <div className="card-body bg-info">
+                        <div className="rotate">
+                          <i className="fab fa-twitter fa-4x"></i>
+                        </div>
+                        <h6 className="text-uppercase">Tweets</h6>
+                        <h1 className="display-4">125</h1>
+                    </div>
+                </div>
+            </div>
+            <div className="col-xl-3 col-sm-6 py-2">
+                <div className="card text-white bg-warning h-40">
+                    <div className="card-body">
+                        <div className="rotate">
+                            <i className="fa fa-share fa-4x"></i>
+                        </div>
+                        <h6 className="text-uppercase">Shares</h6>
+                        <h1 className="display-4">36</h1>
+                    </div>
+                </div>
+            </div>
         </div>
 
-       
-        <div className="col-lg-5 col-md-6 col-sm-12 col-sm-offset-5">
-                  <div className="mb-5" style={{height:"100px",width:"200px"}}><PieChart/> 
+        <div className="row ">
+            <div className="col-lg-7 col-md-6 col-sm-12">
+              <h5 className="mt-3 mb-3 text-secondary">
+               Check More Records of Employees
+              </h5>
+                <div className="table-responsive">
+                    <table className="table table-striped">
+                        <thead className="thead-light">
+                            <tr>
+                                <th>No</th>
+                                <th>Label</th>
+                                <th>Header</th>
+                                <th>Column</th>
+                                <th>Record Data</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                         {record.slice(0, 5).map((output)=>
+                            <tr>
+                                <td>{output.id}</td>
+                                <td>{output.name}</td>
+                                <td>{output.email}</td>
+                                <td>{output.username}</td>
+                                <td>{output.website}</td>
+                                <td></td>
+                            </tr>
+                           )}
+                        </tbody>
+                    </table>
                 </div>
+            </div>
+            <div className="col-lg-5 col-md-6 col-sm-12 col-sm-offset-5">
+                <h4 className='title mt-3 mb-3 text-center text-secondary'>Data in Chart</h4>
+                 <div className="mb-5" style={{height:"300px",width:"400px"}}><PieChart/> </div></div>
         </div>
     </div>
    )
